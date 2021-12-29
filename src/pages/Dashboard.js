2208653e867befbb49c18381918d5e3eeeb6adf1
Divyansh <divyansh.yadav@gmail.com>
2021-12-29 02:40:59 +0000

    cleanup | fix bugs

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useAuth } from "../utils/auth";
 import useSocket from "../utils/socket";
 import Header from "../components/Header";
@@ -16,159 +16,225 @@ export default function Dashboard() {
 	const [conversation, setConversation] = React.useState({});
 	const [loading, setLoading] = React.useState(true);
 
-	function addMessage(message, userId) {
-		setConversation((conversation) => {
-			return {
+	const getUserId = useCallback(
+		(message) => {
+			if (message.from === user.id) {
+				return message.to;
+			}
+
+			return message.from;
+		},
+		[user]
+	);
+
+	const addMessage = useCallback(
+		(message) => {
+			const userId = getUserId(message);
+
+			setConversation((conversation) => ({
 				...conversation,
 				[userId]: [...(conversation[userId] || []), message],
-			};
-		});
-	}
+			}));
+		},
+		[getUserId, setConversation]
+	);
 
-	function updateMessage(message, userId) {
-		setConversation((conversation) => {
-			return {
-				...conversation,
-				[userId]: [
-					...(conversation[userId].map((m) => {
+	const updateMessage = useCallback(
+		(message) => {
+			const userId = getUserId(message);
+
+			setConversation((conversation) => {
+				const messages =
+					conversation[userId].map((m) => {
 						if (m.id === message.id) {
 							return { ...m, ...message };
 						}
 						return m;
-					}) || []),
-				],
-			};
-		});
-	}
+					}) || [];
+
+				return {
+					...conversation,
+					[userId]: messages,
+				};
+			});
+		},
+		[getUserId, setConversation]
+	);
+
+	const updateOrAddMessage = useCallback(
+		(message) => {
+			const userId = getUserId(message);
+			const messages = conversation[userId] || [];
+
+			if (messages.find((m) => m.id === message.id)) {
+				updateMessage(message);
+			} else {
+				addMessage(message);
+			}
+		},
+		[getUserId, addMessage, updateMessage, conversation]
+	);
+
+	const updateMessageCounter = useCallback(
+		(message) => {
+			if (message.from === selectedUserId) {
+				return;
+			}
+
+			setUsers((users) => {
+				return users.map((u) =>
+					u.id === message.from && message.from !== selectedUserId
+						? { ...u, newMessages: (u.newMessages || 0) + 1 }
+						: u
+				);
+			});
+		},
+		[setUsers, selectedUserId]
+	);
 
 	useEffect(async () => {
 		const users = await get(process.env.API_URL + "/users");
-		setUsers(users.filter((u) => u.id !== user.id));
-
 		const conversations = await get(
 			process.env.API_URL + "/users/conversations/" + user.id
 		);
+
+		setUsers(
+			users
+				.filter((u) => u.id !== user.id)
+				.map((user) => {
+					const messages = conversations[user.id] || [];
+					const unseenMessages = messages.reduce((acc, message) => {
+						if (!message.seenByUser) return 1 + acc;
+						return acc;
+					}, 0);
+
+					return {
+						...user,
+						newMessages: unseenMessages,
+					};
+				})
+		);
 		setConversation(conversations);
 		setLoading(false);
-	}, []);
+	}, [setConversation, setLoading, setUsers]);
+
+	useEffect(() => {
+		if (!socket) return;
+
+		function handleConnect(newUser) {
+			// if logged in user connects back
+			if (user.id === newUser.id) {
+				// do nothing
+			}
+			// if the existing using comes online again.
+			else if (users.find((u) => u.id === newUser.id)) {
+				setUsers(
+					users.map((u) => (u.id === newUser.id ? { ...u, status: "online" } : u))
+				);
+			} else {
+				setUsers([...users, newUser]);
+			}
+		}
+
+		function handleDisconnect(user) {
+			setUsers(
+				users.map((u) => (u.id === user.id ? { ...u, status: "offline" } : u))
+			);
+		}
+
+		socket.on("user connect", handleConnect);
+		socket.on("user disconnect", handleDisconnect);
+
+		return () => {
+			socket.off("user connect", handleConnect);
+			socket.off("user disconnect", handleDisconnect);
+		};
+	}, [socket, user, users, setUsers]);
 
 	useEffect(() => {
 		if (loading) return;
 
-		console.log("setting up socket listeners", socket);
-		// socket.auth = user;
-		socket.on("user connected", (newUser) => {
-			if (newUser === null) return;
-			setUsers((users) => {
-				if (user.id === newUser.id || users.find((u) => u.id === newUser.id)) {
-					return users.map((u) => {
-						if (u.id === newUser.id) {
-							return { ...u, status: "online" };
-						}
-						return u;
-					});
-				}
-
-				return [...users, newUser];
-			});
-		});
-
-		socket.on("private message", (message) => {
-			if (message.from !== selectedUserId) {
-				setUsers((users) => {
-					return users.map((u) =>
-						u.id === message.from && message.from !== selectedUserId
-							? { ...u, newMessages: (u.newMessages || 0) + 1 }
-							: u
-					);
-				});
-			} else {
-				// message.seenByUser = true;
-			}
-
+		function handleMessage(message) {
 			if (document.visibilityState === "hidden") {
 				beep();
 			}
 
+			if (message.from === selectedUserId) {
+				message.seenByUser = true;
+			}
+
+			updateMessageCounter(message);
 			message.reachedToUser = true;
-			addMessage(message, message.from);
-			socket.emit("private message reached to user", message);
-		});
+			addMessage(message);
+			socket.emit("message ack", message);
+		}
 
-		socket.on("update private message", (message) => {
-			addMessage(message, message.to);
-		});
-
-		socket.on("update private message again", (message) => {
-			updateMessage(message, message.to);
-		});
-
-		socket.on("private message reached to user", (message) => {
-			updateMessage(message, message.to);
-		});
-
-		socket.on("update private message reached to user", (message) => {
-			updateMessage(message, message.from);
-		});
-
-		socket.on("user disconnected", (user) => {
-			setUsers((users) =>
-				users.map((u) => (u.id === user.id ? { ...u, status: "offline" } : u))
-			);
-		});
-
-		socket.on("message seen by user", (message) => {
-			updateMessage(message, message.to);
-		});
-
-		socket.on("update message seen by user", (message) => {
-			updateMessage(message, message.from);
-		});
+		socket.on("message", handleMessage);
+		socket.on("message ack", updateMessage);
+		socket.on("message seen", updateMessage);
+		socket.on("add/update message", updateOrAddMessage);
 
 		return () => {
-			socket.removeAllListeners();
+			socket.off("message", handleMessage);
+			socket.off("message ack", updateMessage);
+			socket.off("message seen", updateMessage);
+			socket.off("add/update message", updateOrAddMessage);
 		};
-	}, [user, loading, selectedUserId, socket]);
+	}, [
+		loading,
+		socket,
+		selectedUserId,
+		addMessage,
+		updateMessage,
+		updateMessageCounter,
+	]);
 
-	// useEffect(() => {
-	// 	const messages = conversation[selectedUserId] || [];
+	useEffect(() => {
+		if (!socket) return;
 
-	// 	messages
-	// 		.filter((m) => m.from === selectedUserId)
-	// 		.forEach((m) => {
-	// 			if (m.seenByUser !== undefined && m.seenByUser === false) {
-	// 				socket.emit("message seen by user", m);
-	// 			}
-	// 		});
-	// }, [conversation, selectedUserId]);
+		setConversation((conversation) => {
+			const messages = conversation[selectedUserId] || [];
 
-	// useEffect(() => {
-	// 	socket.on("private message", (message) => {
-	// 		if (message.from !== selectedUserId) {
-	// 			setUsers((users) => {
-	// 				return users.map((u) =>
-	// 					u.id === message.from && message.from !== selectedUserId
-	// 						? { ...u, newMessages: (u.newMessages || 0) + 1 }
-	// 						: u
-	// 				);
-	// 			});
-	// 		} else {
-	// 			message.seenByUser = true;
-	// 		}
+			return {
+				...conversation,
+				[selectedUserId]: messages.map((message) => {
+					if (!message.seenByUser) {
+						message.seenByUser = true;
 
-	// 		if (document.visibilityState === "hidden") {
-	// 			beep();
-	// 		}
+						socket.emit("message seen", message);
 
-	// 		message.reachedToUser = true;
-	// 		addMessage(message, message.from);
-	// 		socket.emit("private message reached to user", message);
-	// 	});
+						return {
+							...message,
+						};
+					}
 
-	// 	return () => {
-	// 		socket.removeAllListeners("private message");
-	// 	};
-	// }, [selectedUserId]);
+					return message;
+				}),
+			};
+		});
+	}, [socket, selectedUserId, setConversation]);
+
+	function sendMessage(text) {
+		const message = {
+			id: Math.random().toString(),
+			text,
+			to: selectedUserId,
+			toUser: users.find((u) => u.id === selectedUserId),
+			from: user.id,
+			fromUser: user,
+			reachedToServer: false,
+			reachedToUser: false,
+			seenByUser: false,
+			timestamp: new Date().getTime(),
+		};
+
+		addMessage(message);
+		socket.emit("message", message, (msg) => {
+			updateMessage(msg);
+		});
+	}
+
+	if (loading) return <div>Please wait...</div>;
 
 	return (
 		<div
@@ -197,24 +263,7 @@ export default function Dashboard() {
 					<Chat
 						userId={user.id}
 						messages={conversation[selectedUserId] || []}
-						onSendMessage={(text) => {
-							const message = {
-								id: Math.random().toString(),
-								text,
-								to: selectedUserId,
-								toUser: users.find((u) => u.id === selectedUserId),
-								from: user.id,
-								fromUser: user,
-								reachedToServer: false,
-								reachedToUser: false,
-								seenByUser: false,
-							};
-
-							addMessage(message, message.to);
-							socket.emit("private message", message, (msg) => {
-								updateMessage(msg, msg.to);
-							});
-						}}
+						onSendMessage={sendMessage}
 					/>
 				)}
 			</div>
