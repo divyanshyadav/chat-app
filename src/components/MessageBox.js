4d324ecc94ae40dbeb02e224d3ca5f5a0ec9a7a7
Divyansh <divyansh.yadav@gmail.com>
2021-12-29 03:14:39 +0000

    add time to message

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -18,62 +18,103 @@ export default function MessagesBox({ userId, messages }) {
 			}}
 		>
 			{messages.map((message) => (
-				<div
-					key={message.id}
-					style={{
-						display: "flex",
-						justifyContent: message.from === userId ? "flex-end" : "flex-start",
-					}}
-				>
+				<Message key={message.id} message={message} userId={userId} />
+			))}
+		</div>
+	);
+}
+
+function Message({ message, userId }) {
+	const isFromLoggedInUser = message.from === userId;
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: isFromLoggedInUser ? "flex-end" : "flex-start",
+			}}
+		>
+			<div
+				style={{
+					textAlign: isFromLoggedInUser ? "right" : "left",
+					backgroundColor:
+						message.from === userId ? "rgb(233 207 233)" : "rgb(231 140 231)",
+					padding: "5px",
+					borderRadius: "5px",
+					margin: "5px",
+					display: "flex",
+					flexDirection: "row",
+				}}
+			>
+				<UserImage url={message.fromUser.imageUrl} />
+				<div>
+					<strong>{message.fromUser.name}</strong>
+					<br />
 					<div
 						style={{
-							textAlign: message.from === userId ? "right" : "left",
-							backgroundColor:
-								message.from === userId ? "rgb(233 207 233)" : "rgb(231 140 231)",
-							padding: "5px",
-							borderRadius: "5px",
-							margin: "5px",
-							display: "flex",
-							flexDirection: "row",
+							fontSize: "18px",
+							marginTop: "5px",
 						}}
 					>
-						<UserImage url={message.fromUser.imageUrl} />
-						<div>
-							<strong>{message.fromUser.name}</strong>
-							<br />
-							<div
-								style={{
-									fontSize: "18px",
-									marginTop: "5px",
-								}}
-							>
-								{message.text}
-							</div>
+						{message.text}
+					</div>
+					<div
+						style={{
+							textAlign: "right",
+						}}
+					>
+						<Time timestamp={message.timestamp} />
+						{isFromLoggedInUser && (
 							<MessageStatus
 								reachedToServer={message.reachedToServer}
 								reachedToUser={message.reachedToUser}
 								seenByUser={message.seenByUser}
 							/>
-						</div>
+						)}
 					</div>
 				</div>
-			))}
+			</div>
+		</div>
+	);
+}
+
+function Time({ timestamp }) {
+	function formatAMPM(date) {
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var ampm = hours >= 12 ? "pm" : "am";
+		hours = hours % 12;
+		hours = hours ? hours : 12; // the hour '0' should be '12'
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		var strTime = hours + ":" + minutes + " " + ampm;
+		return strTime;
+	}
+
+	return (
+		<div
+			style={{
+				display: "inline-block",
+				marginRight: "5px",
+				fontSize: "12px",
+			}}
+		>
+			{formatAMPM(new Date(timestamp))}
 		</div>
 	);
 }
 
 function MessageStatus({ reachedToServer, reachedToUser, seenByUser }) {
 	if (seenByUser) {
-		return <div>👁</div>;
+		return "👀";
 	}
 
 	if (reachedToUser) {
-		return <div>✓✓</div>;
+		return "✔️✔️";
 	}
 
 	if (reachedToServer) {
-		return <div>✓</div>;
+		return "✔️";
 	}
 
-	return "⏰";
+	return "🕑";
 }
